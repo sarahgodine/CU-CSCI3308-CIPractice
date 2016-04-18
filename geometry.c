09ef92a09db8c82251499bd02067a2437849b6bc
Sarah <sarah.godine@colorado.edu>
2016-04-18 16:10:16 -0600

    geometry

--- geometry.c
+++ geometry.c
@@ -69,3 +69,7 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+double coord_2d_area_triangle(const coord_2d_t *a, const coord_2d_t *b, const coord_2d_t *c) {
+    area = (((a->x)(b->y - c->y) + (b->x)(a->y - c->y) + (c->x)(a->y - b->y)) / 2.0 );
+}
